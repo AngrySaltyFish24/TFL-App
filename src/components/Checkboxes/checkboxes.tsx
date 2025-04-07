e19846011a6e34407df19212beef41bfbe582162
AngrySaltyFish24 <33280495+sammo4gun@users.noreply.github.com>
2025-04-07 12:04:02 +0200

    Adds the Checkbox component
    This seperate out the logic for Checkbox in the filter panal to a
    seperate component. This helps with reuseability and seperation of
    concerns.

---
+++ src/components/Checkboxes/checkboxes.tsx
@@ -0,0 +1,95 @@
+import { Predicate, Tube } from "types";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+import {
+  useCallback,
+  useMemo,
+} from "react";
+
+import { useState } from "react";
+const makeFilterFunc = (name: string) => {
+  return (tube: Tube) => tube.status.description === name;
+};
+
+type CheckboxState = {
+  [key: string]: {
+    checked: boolean;
+    filterFunc: Predicate<Tube>;
+  };
+};
+
+export const useCheckboxes = (
+  names: string[],
+): [
+  CheckboxState,
+  () => Predicate<Tube>[],
+  (name: string, value: boolean) => void,
+] => {
+  const defaultCheckboxes: CheckboxState = useMemo(() => {
+    const result: CheckboxState = {};
+    names.forEach((value) => {
+      result[value] = {
+        checked: true,
+        filterFunc: makeFilterFunc(value),
+      };
+    });
+    return result;
+  }, [names]);
+
+  const [checkboxes, setCheckboxes] =
+    useState<CheckboxState>(defaultCheckboxes);
+
+  const updateCheckboxState = useCallback((name: string, value: boolean) => {
+    setCheckboxes((current) => {
+      const newValue: CheckboxState = {};
+      newValue[name] = Object.assign(current[name], {
+        checked: value,
+      });
+
+      return {
+        ...current,
+        ...newValue,
+      };
+    });
+  }, []);
+
+  const currentActiveFilters = () => {
+    const activeCheckboxFilters = names
+      .map((name) => checkboxes[name])
+      .filter((checkboxStatus) => checkboxStatus["checked"])
+      .map((checkbox) => checkbox["filterFunc"]);
+
+    return activeCheckboxFilters;
+  };
+
+  return [checkboxes, currentActiveFilters, updateCheckboxState];
+};
+type CheckboxProps = {
+  checkboxState: CheckboxState;
+  updateCheckboxState: (name: string, value: boolean) => void;
+  onCheckboxChange: () => void;
+  checkboxKey: string;
+};
+const Checkboxes = ({ checkboxState, updateCheckboxState }: CheckboxProps) => {
+  return (
+    <>
+      {Object.keys(checkboxState).map((item) => (
+        <FormControlLabel
+          key={item}
+          control={
+            <Checkbox
+              checked={checkboxState[item]["checked"]}
+              onChange={(event) => {
+                updateCheckboxState(item, event.target.checked);
+              }}
+            />
+          }
+          label={item}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Checkboxes;
---
+++ src/components/Checkboxes/index.tsx
@@ -0,0 +1,6 @@
+import Checkboxes, { useCheckboxes } from "./checkboxes";
+
+export {
+    useCheckboxes
+}
+export default Checkboxes;
